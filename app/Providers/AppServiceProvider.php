861c3734f296e7a8527252f7822ec571e6ca1b51
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2018-11-27 11:34:26 +0000

    Basic user profile editing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        //Carbon 是 PHP 知名的 DateTime 操作扩展，Laravel 将其默认集成到了框架中。
+        //diffForHumans 是 Carbon 对象提供的方法，
+        //默认情况是英文的，如果要使用中文时间提示，
+        //则需要对 Carbon 进行本地化设置。
+        //对 Carbon 进行本地化的设置很简单，
+        //只在 AppServiceProvider 中调用 Carbon 的 setLocale 方法即可，
+        //AppServiceProvider 是框架的核心，在 Laravel 启动时，会最先加载该文件。
+        \Carbon\Carbon::setLocale('zh');
     }
 
     /**
