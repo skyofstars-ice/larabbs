87729a9db099f888ce263b05f21eb2c47c9722ee
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2018-11-27 12:55:53 +0000

    WYSIWYG Editor

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 let mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +11,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+    .copyDirectory('resources/assets/editor/js', 'public/js')
+    .copyDirectory('resources/assets/editor/css', 'public/css')
+;
