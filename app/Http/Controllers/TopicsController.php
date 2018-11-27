48e45c2e6263d7eae020b05d5370cbf45cb544c6
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2018-11-27 13:49:57 +0000

    Add queue for translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -40,7 +40,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('success', '创建成功！');
+        return redirect()->to($topic->link())->with('success', '创建成功！');
     }
 
     public function edit(Topic $topic)
@@ -55,7 +55,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
+        return redirect()->to($topic->link())->with('success', '更新成功！');
     }
 
     public function destroy(Topic $topic)
