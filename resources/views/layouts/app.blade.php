96210844352e746801a0e99cc86d25331df654a2
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2018-11-28 14:42:41 +0000

    Admin can manage site settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,10 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('title', 'LaraBBS') - 测试中</title>
-    <meta name="description" content="@yield('description', '测试')" />
+    
+    <title>@yield('title', 'LaraBBS') - {{ setting('site_name', '测试中') }}</title>
+    <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS 爱好者社区。'))" />
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
